28c4fce3b3c3d88c7076d5d9f85cef08a9f1924f
tianheg <yidajiabei@163.com>
2022-05-03 17:37:53 +0800

    remove: comment

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,7 @@
 $.getJSON("words.json", function (data) {
-    let randomWords = data.data[Math.ceil(Math.random() * data.data.length) - 1]; // get each JSON object
-    console.log(data.data[0])
-  // console.log(Math.ceil(Math.random() * data.length));
+  let randomWords = data.data[Math.ceil(Math.random() * data.data.length) - 1];
   document.getElementById("words").innerText = randomWords.word;
   document.getElementById("exp").innerHTML = randomWords.exp;
-  // console.log(randomBlog.name);
-  // console.log("https://www.yidajiabei.xyz/blog/" + randomBlog.link);
 });
 
 const btn = document.querySelector("#btn")
